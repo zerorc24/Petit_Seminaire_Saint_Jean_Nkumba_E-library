af8d46b20d3d6bec30670dc4e80e892d521ca0a0
zerorc24 <leomugisha84@gmail.com>
2026-05-08 13:35:07 +0200

    Admin added

--- core/settings.py
+++ core/settings.py
@@ -2,24 +2,29 @@ from pathlib import Path
 import os
 import dj_database_url
 
-# -------------------------------
-# BASE DIR
-# -------------------------------
+# -----------------------------------
+# BASE DIRECTORY
+# -----------------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# -------------------------------
+# -----------------------------------
 # SECURITY
-# -------------------------------
-SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
+# -----------------------------------
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "petit-seminaire-saint-jean-nkumba-e.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
-# -------------------------------
-# APPLICATIONS
-# -------------------------------
+
+# -----------------------------------
+# INSTALLED APPS
+# -----------------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -33,9 +38,9 @@ INSTALLED_APPS = [
 ]
 
 
-# -------------------------------
+# -----------------------------------
 # MIDDLEWARE
-# -------------------------------
+# -----------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -49,9 +54,9 @@ MIDDLEWARE = [
 ]
 
 
-# -------------------------------
-# AUTH SYSTEM (IMPORTANT)
-# -------------------------------
+# -----------------------------------
+# AUTH SYSTEM
+# -----------------------------------
 AUTH_USER_MODEL = 'library.CustomUser'
 
 AUTHENTICATION_BACKENDS = [
@@ -59,25 +64,31 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = "login"
+
 LOGIN_REDIRECT_URL = "dashboard"
+
 LOGOUT_REDIRECT_URL = "home"
 
 
-# -------------------------------
+# -----------------------------------
 # URL CONFIG
-# -------------------------------
+# -----------------------------------
 ROOT_URLCONF = 'core.urls'
+
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# -------------------------------
+# -----------------------------------
 # TEMPLATES
-# -------------------------------
+# -----------------------------------
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+
         'DIRS': [BASE_DIR / 'templates'],
+
         'APP_DIRS': True,
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
@@ -89,104 +100,137 @@ TEMPLATES = [
 ]
 
 
-# -------------------------------
+# -----------------------------------
 # DATABASE
-# -------------------------------
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
+# -----------------------------------
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-
-# -------------------------------
+# -----------------------------------
 # PASSWORD VALIDATION
-# -------------------------------
+# -----------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
+    },
 ]
 
 
-# -------------------------------
+# -----------------------------------
 # INTERNATIONALIZATION
-# -------------------------------
+# -----------------------------------
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_TZ = True
 
 
-# -------------------------------
+# -----------------------------------
 # STATIC FILES
-# -------------------------------
+# -----------------------------------
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# -------------------------------
+# -----------------------------------
 # MEDIA FILES
-# -------------------------------
+# -----------------------------------
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-# -------------------------------
-# EMAIL (DEV MODE)
-# -------------------------------
-
+# -----------------------------------
+# EMAIL SETTINGS
+# -----------------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
+
 EMAIL_PORT = 587
+
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'pssjnteam@gmail.com'
-EMAIL_HOST_PASSWORD = 'hnhb adoe kcqk ypmz'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
-# -------------------------------
-# SECURITY (DEV SAFE)
-# -------------------------------
+# -----------------------------------
+# SECURITY SETTINGS
+# -----------------------------------
 CSRF_COOKIE_SECURE = True
+
 SESSION_COOKIE_SECURE = True
 
-# -------------------------------
-# DEFAULT AUTO FIELD
-# -------------------------------
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# -------------------------------
-# CSRF TRUST
-# -------------------------------
+# -----------------------------------
+# CSRF TRUSTED ORIGINS
+# -----------------------------------
 CSRF_TRUSTED_ORIGINS = [
     "https://petit-seminaire-saint-jean-nkumba-e.onrender.com",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
 ]
-PWA_APP_SPLASH_SCREEN = [
-    {
-        'src': '/static/images/icon-512.png',
-        'media': '(device-width: 320px) and (device-height: 568px)'
-    }
-]
 
-PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'en-US'
+
+# -----------------------------------
+# DEFAULT AUTO FIELD
+# -----------------------------------
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# -----------------------------------
+# PWA SETTINGS
+# -----------------------------------
 PWA_APP_NAME = 'Petit Seminaire'
+
 PWA_APP_DESCRIPTION = "Digital Library System"
+
 PWA_APP_THEME_COLOR = '#000000'
+
 PWA_APP_BACKGROUND_COLOR = '#ffffff'
+
 PWA_APP_DISPLAY = 'standalone'
+
 PWA_APP_SCOPE = '/'
+
 PWA_APP_ORIENTATION = 'portrait'
+
 PWA_APP_START_URL = '/'
+
 PWA_APP_STATUS_BAR_COLOR = 'default'
 
 PWA_APP_ICONS = [
@@ -200,5 +244,13 @@ PWA_APP_ICONS = [
     }
 ]
 
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/images/icon-512.png',
+        'media': '(device-width: 320px) and (device-height: 568px)'
+    }
+]
+
 PWA_APP_DIR = 'ltr'
+
 PWA_APP_LANG = 'en-US'
