d1127714ef56ef2408757dff128087fb960f5406
zerorc24 <leomugisha84@gmail.com>
2026-05-10 12:42:33 +0200

    Enable debug for Render troubleshooting

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = False  # ALWAYS False on Render (set True only locally)
+DEBUG = True  # ALWAYS False on Render (set True only locally)
 
 ALLOWED_HOSTS = [
     "petit-seminaire-saint-jean-nkumba-e.onrender.com",
